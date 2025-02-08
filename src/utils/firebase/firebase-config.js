a23c897dcc90c2b9d0ce7fb509d313f04ce0c3a7
SuleimanovErik <eriksuleimanov109@gmail.com>
2025-02-08 17:26:27 +0100

    add authentication

---
+++ src/utils/firebase/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEaNze5wjmP4CRCYWDpL31yVYGo2cjk00",
+  authDomain: "phoenix-kitchen.firebaseapp.com",
+  projectId: "phoenix-kitchen",
+  storageBucket: "phoenix-kitchen.firebasestorage.app",
+  messagingSenderId: "163516191772",
+  appId: "1:163516191772:web:e3d9b459d76b736f94bbd9",
+  measurementId: "G-W29JXN7ZPK"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+export const db=getFirestore(app)
