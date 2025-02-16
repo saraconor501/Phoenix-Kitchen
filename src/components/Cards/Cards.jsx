718e4570ee0bbf521957ef186975d67583c53e0c
SuleimanovErik <eriksuleimanov109@gmail.com>
2025-02-16 08:53:03 +0100

    add skeleton

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,53 +1,70 @@
-
-import React, { useEffect } from "react";
-import useProducts from "../../store/products-slice/products-slice";
-import main from "./Cards.module.css";
+import React, { useEffect } from 'react';
+import useProducts from '../../store/products-slice/products-slice';
+import main from './Cards.module.css';
+import { Skeleton } from 'antd';
+import CardSkeleton from '../Card-Skeleton/Card-Skeleton';
 
 const Cards = () => {
-  const { products, fetchProducts } = useProducts();
+  const { products, fetchProducts, isLoading, error } = useProducts();
 
   useEffect(() => {
     fetchProducts();
-
   }, [fetchProducts]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <>
-
       <div className={main.productsGrid}>
-        {products.map((item, index) => (
-          <div key={index} className={main.productCard}>
+        {isLoading? 
+            Array.from({ length: 6 }).map((_, index) => <CardSkeleton key={index} />)
+          : 
+            products.map((item, index) => (
+              <div key={index} className={main.productCard}>
+                <div className={main.imageContainer}>
+                  <img style={{ width: 'auto' }} src={item.imageUrl} alt={item.name} />
+                  {item.isAvailable && (
+                    <div className={main.statusBadge}>
+                      <div className={main.open}></div> Open
+                    </div>
+                  )}
+                  {!item.isAvailable && (
+                    <div className={main.statusBadgeNone}>
+                      <div className={main.close}></div> Close
+                    </div>
+                  )}
+                </div>
 
-            <div className={main.imageContainer}>
-              <img style={{width:"auto"}} src={item.imageUrl} alt={item.name} />
-              {item.isAvailable && <div className={main.statusBadge}><div className={main.open}></div> Open</div>}
-              {!item.isAvailable && <div className={main.statusBadgeNone}><div className={main.close}></div> Close</div>}
-              {console.log(item.isAvailable)}
-            </div>
-
-            <div className={main.productInfo}>
-              <div className={main.rating}>
-                <span className={main.star}>⭐</span>
-                <span >{item?.rating}  </span>  {item.isAvailable? 'Dining & Delivery': 'Dining'}
+                <div className={main.productInfo}>
+                  <div className={main.rating}>
+                    <span className={main.star}>⭐️</span>
+                    <span>{item?.rating}</span> {item.isAvailable ? 'Dining & Delivery' : 'Dining'}
+                  </div>
+                  <h2 className={main.productName}>{item.name}</h2>
+                  <div className={main.titleProduct}>
+                    <p className={main.deliveryTime}>
+                      <img
+                        style={{ width: '30px' }}
+                        src="https://cdn-icons-png.flaticon.com/512/1023/1023346.png"
+                      />{' '}
+                      Вес: {item.weight} г
+                    </p>
+                    <p className={main.restaurantName}>
+                      <img src="/images/product-icon.svg" /> {item.restauran || 'Not restauran'}
+                    </p>
+                  </div>
+                  <p className={main.price}>{item.price} coм</p>
+                  <div className={main.buttons}>
+                    <button className={main.saveButton}>
+                      <img src="/images/icon-save.svg" /> Save for later
+                    </button>
+                    <button className={main.addToCartButton}>Add to cart</button>
+                  </div>
+                </div>
               </div>
-              <h2 className={main.productName}>{item.name}</h2>
-              <div className={main.titleProduct}>
-              <p className={main.deliveryTime}> <img style={{ width: '30px' }} src="https://cdn-icons-png.flaticon.com/512/1023/1023346.png" /> Вес:  {item.weight} г</p>
-
-                <p className={main.restaurantName}>
-                  <img src="/images/product-icon.svg" /> {item.restauran || "Pizza Heaven"}
-                </p>
-        
-              </div>
-
-              <p className={main.price}>{item.price} coм</p>
-              <div className={main.buttons}>
-                <button className={main.saveButton}><img src="/images/icon-save.svg"/> Save for later</button>
-                <button className={main.addToCartButton}> Add to cart</button>
-              </div>
-            </div>
-          </div>
-        ))}
+            ))}
       </div>
     </>
   );
--- src/store/products-slice/products-slice.js
+++ src/store/products-slice/products-slice.js
@@ -3,18 +3,45 @@ import { db } from "../../utils/firebase/firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 const useProducts = create((set) => ({
-  products: [],
-  // Функция для получения продуктов из Firestore
+  products: [], 
+  isLoading: false, 
+  error: null, 
+
   fetchProducts: async () => {
+    set({ isLoading: true, error: null }); 
+
     try {
-      // Путь: restaurants/ImpirePizza/menu
+      const cachedData = localStorage.getItem('cachedProducts');
+      if (cachedData) {
+        const { products, timestamp } = JSON.parse(cachedData);
+        const isCacheValid = (Date.now() - timestamp) < 3600000; 
+
+        if (isCacheValid) {
+          set({ products, isLoading: false })
+          return;
+        }
+      }
       const menuCollectionRef = collection(db, "restaurants", "ImpirePizza", "menu");
       const snapshot = await getDocs(menuCollectionRef);
       const products = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      set({ products });
+
+      localStorage.setItem('cachedProducts', JSON.stringify({
+        products,
+        timestamp: Date.now()
+      }));
+
+
+      set({ products, isLoading: false });
     } catch (error) {
       console.error("Ошибка при получении продуктов:", error);
+      set({ error: "Не удалось загрузить продукты", isLoading: false });
     }
+  },
+
+  
+  clearCache: () => {
+    localStorage.removeItem('cachedProducts');
+    set({ products: [] });
   }
 }));
 
